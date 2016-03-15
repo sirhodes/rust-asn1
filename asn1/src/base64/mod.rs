2de04f9e5bd1772df999db045933ca97d3bb6737
Adam Crain <jadamcrain@gmail.com>
2016-03-15 15:12:28 -0400

    changed exports

---
+++ asn1/src/base64/mod.rs
@@ -0,0 +1,5 @@
+pub use self::decode::{decode};
+pub use self::encode::{encode, encode_as_string};
+
+mod encode;
+mod decode;
--- asn1/src/lib.rs
+++ asn1/src/lib.rs
@@ -1,11 +1,6 @@
 pub use self::tag::{TypeId, Tag, Class};
 pub use self::length::{read_len, Length, LengthError};
-pub use self::base64::decode::{decode};
-pub use self::base64::encode::{encode, encode_as_string};
 
 mod tag;
 mod length;
-mod base64 {
-    pub mod encode;
-    pub mod decode;
-}
+pub mod base64;
